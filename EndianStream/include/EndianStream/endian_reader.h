530c552faa155d7b349d4de50b9ac710d47f42aa
Richard Moore <51307516+Zatarita@users.noreply.github.com>
2021-06-14 21:17:28 -0400

    fix tab-whitespace inconsistency

--- EndianStream/include/EndianStream/endian_reader.h
+++ EndianStream/include/EndianStream/endian_reader.h
@@ -13,20 +13,20 @@
 
 namespace SysIO
 {
-	class EndianReader
-	{
-	    public:
-	    // Exceptions/Constraints
-	    static constexpr const char* EXCEPTION_FILE_ACCESS {"Unable To Access Requested File."};
-	    static constexpr const char* EXCEPTION_FILE_BOUNDS {"Requested Offset Exceeds Bounds Of The File."};
+    class EndianReader
+    {
+    public:
+        // Exceptions/Constraints
+        static constexpr const char* EXCEPTION_FILE_ACCESS {"Unable To Access Requested File."};
+        static constexpr const char* EXCEPTION_FILE_BOUNDS {"Requested Offset Exceeds Bounds Of The File."};
         static const uint16_t        MAXIMUM_STRING_LENGTH {0xffff};
 
-	    size_t        fileSize {};
-		std::ifstream file {};
-		ByteOrder     fileEndianness {};
-	public:
-		EndianReader(const std::string&, const ByteOrder&);
-		~EndianReader();
+        size_t        fileSize {};
+        std::ifstream file {};
+        ByteOrder     fileEndianness {};
+    public:
+        EndianReader(const std::string&, const ByteOrder&);
+        ~EndianReader();
 
         void   seek(const size_t&);                      // Goto a specific offset
         void   pad (const size_t&);                      // Burn padding
@@ -38,37 +38,37 @@ namespace SysIO
         ByteArray readRaw(const size_t&, const size_t&); // Seek, read, and seek back
         ByteArray readRaw(const size_t&);                // Read from current position
 
-		// Template Functions
-		template <class type>
-		type read()
-		{
-		    // Read data of a certain type and return it.
-		    type ret;
-		    *this >> ret;
+        // Template Functions
+        template <class type>
+        type read()
+        {
+            // Read data of a certain type and return it.
+            type ret;
+            *this >> ret;
             return ret;
-		}
+        }
 
-		template <class type>
-		type peek()
-		{
-		    // Read data of a certain type, seek the stream back, and return the data
-		    type ret;
-		    *this >> ret;
+        template <class type>
+        type peek()
+        {
+            // Read data of a certain type, seek the stream back, and return the data
+            type ret;
+            *this >> ret;
 
-		    seek( tell() - sizeof(type) );
+            seek( tell() - sizeof(type) );
 
-		    return ret;
-		}
+            return ret;
+        }
 
-		template <class type>
-		EndianReader& operator>>(type& data)
-		{
-		    // Read data from the stream, swap the endianness if needed.
-			file.read(reinterpret_cast<char*>( &data ), sizeof(data));
-			if (SysIO::systemEndianness != fileEndianness)
-				SysIO::EndianSwap(data);
+        template <class type>
+        EndianReader& operator>>(type& data)
+        {
+            // Read data from the stream, swap the endianness if needed.
+            file.read(reinterpret_cast<char*>( &data ), sizeof(data));
+            if (SysIO::systemEndianness != fileEndianness)
+                SysIO::EndianSwap(data);
             return *this;
-		};
-	};
+        };
+    };
 }
 #endif
